7a36477f6d4a78228d5e4baa5363e98083767c21
Abdollah Raafat <80994020+AbdollahRaafat@users.noreply.github.com>
2022-01-13 23:03:47 +0200

    Update path by - path: '/' -

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: '/',
   },
   devtool: 'inline-source-map',
   devServer: {
